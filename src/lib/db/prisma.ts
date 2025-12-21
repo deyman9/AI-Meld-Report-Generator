6691429620cef95952909f7068b13461b7157743
deyman9 <daniel.eyman@gmail.com>
2025-12-21 08:20:04 -0700

    Fix NextAuth and Prisma versions for compatibility (v4 and v5)

--- src/lib/db/prisma.ts
+++ src/lib/db/prisma.ts
@@ -1,10 +1,13 @@
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined }
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
+
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export default prisma
-
